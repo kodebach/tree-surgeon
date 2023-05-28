822722c7aa19e2f1fa92cfecfa3a559f3b999878
Klemens Böswirth <klemens96+git@gmail.com>
2023-05-28 17:24:54 +0200

    test: add tests for lexer

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -1,5 +1,6 @@
-use chumsky::{span::SimpleSpan};
+use chumsky::span::SimpleSpan;
 use logos::{Lexer, Logos};
+use rstest::rstest;
 
 #[derive(Debug, PartialEq, Clone, Default, thiserror::Error)]
 pub enum TokenError {
@@ -10,7 +11,7 @@ pub enum TokenError {
     Other,
 }
 
-#[derive(Logos, Debug, Clone, PartialEq)]
+#[derive(Logos, Debug, Clone, PartialEq, Eq)]
 #[logos(skip r"\s+", error = TokenError)]
 pub enum Token<'a> {
     #[regex(r"-?[[:digit:]]+(\.[[:digit:]]+)?([eE][+-]?[[:digit:]]+)?", priority = 2, callback = |lex| lex.slice())]
@@ -114,14 +115,14 @@ fn parse_string<'a>(lexer: &mut Lexer<'a, Token<'a>>) -> Result<String, TokenErr
 pub enum StringError {
     #[error("invalid escape sequence '\\{0}'")]
     InvalidEscape(char),
-    #[error("invalid unicode codepoint '\\u{0:#04x}'")]
+    #[error("invalid unicode codepoint '\\u{0:04x?}'")]
     InvalidUnicode(u32),
     #[default]
     #[error("unknown error")]
     Other,
 }
 
-#[derive(Logos, Debug)]
+#[derive(Logos, Debug, PartialEq, Eq)]
 #[logos(error = StringError)]
 enum StringToken {
     #[regex(r#"\\[bfnrt\\/"]"#, parse_escape)]
@@ -131,7 +132,7 @@ enum StringToken {
         parse_unicode
     )]
     Unicode(char),
-    #[regex(r#"[^"\\]"#, |lex| lex.slice().chars().next())]
+    #[regex(r#"[^"\\]"#, parse_other)]
     Other(char),
 }
 
@@ -154,6 +155,13 @@ fn parse_unicode(lexer: &mut Lexer<StringToken>) -> Result<char, StringError> {
     char::from_u32(codepoint).ok_or(StringError::InvalidUnicode(codepoint))
 }
 
+fn parse_other(lexer: &mut Lexer<StringToken>) -> char {
+    let span = lexer.span();
+    let c = lexer.source()[span.start..].chars().next().unwrap();
+    lexer.bump(c.len_utf8() - span.len());
+    c
+}
+
 type SpannedLexerResult<'a> = Vec<(Result<Token<'a>, TokenError>, SimpleSpan)>;
 
 fn run_lexer_impl(source: &str, filter_comments: bool) -> SpannedLexerResult {
@@ -166,4 +174,139 @@ fn run_lexer_impl(source: &str, filter_comments: bool) -> SpannedLexerResult {
 
 pub fn run_lexer(source: &str) -> SpannedLexerResult {
     run_lexer_impl(source, true)
+}
+
+#[rstest]
+#[case(
+    "ñ\u{009f}\u{0098}\u{009c}&L}¦�\x0b\x1b$X\x1b%`ð²\u{009e}\u{008c}/RÂ\u{008c}\"ð´¿\u{0081}\x00\r$�dXåó©\u{0098}®\x08&\"}",
+    vec![
+        Err(TokenError::Other),
+        Err(TokenError::Other),
+        Err(TokenError::Other),
+        Err(TokenError::Other),
+        Err(TokenError::Other),
+        Ok(Token::Identifier("L")),
+        Ok(Token::RBrace),
+        Err(TokenError::Other),
+        Err(TokenError::Other),
+        Err(TokenError::Other),
+        Ok(Token::Dollar),
+        Ok(Token::Identifier("X")),
+        Err(TokenError::Other),
+        Err(TokenError::Other),
+        Err(TokenError::Other),
+        Err(TokenError::Other),
+        Err(TokenError::Other),
+        Err(TokenError::Other),
+        Err(TokenError::Other),
+        Err(TokenError::Other),
+        Ok(Token::Identifier("R")),
+        Err(TokenError::Other),
+        Err(TokenError::Other),
+        Ok(Token::String("ð´¿\u{0081}\x00\r$�dXåó©\u{0098}®\x08&".to_string())),
+        Ok(Token::RBrace),
+    ],
+)]
+#[case(
+    r#"match (translation_unit (declaration declarator: (init_declarator)) @decl) warn @decl "Global variable detected";
+    match (translation_unit (preproc_ifdef (declaration declarator: (init_declarator)) @decl)) warn @decl "Global variable detected";"#,
+    vec![
+        Ok(Token::Match),
+        Ok(Token::LParen),
+        Ok(Token::Identifier("translation_unit")),
+        Ok(Token::LParen),
+        Ok(Token::Identifier("declaration")),
+        Ok(Token::Identifier("declarator")),
+        Ok(Token::Colon),
+        Ok(Token::LParen),
+        Ok(Token::Identifier("init_declarator")),
+        Ok(Token::RParen),
+        Ok(Token::RParen),
+        Ok(Token::Capture("decl")),
+        Ok(Token::RParen),
+        Ok(Token::Warn),
+        Ok(Token::Capture("decl")),
+        Ok(Token::String("Global variable detected".to_string())),
+        Ok(Token::Semicolon),
+
+        Ok(Token::Match),
+        Ok(Token::LParen),
+        Ok(Token::Identifier("translation_unit")),
+        Ok(Token::LParen),
+        Ok(Token::Identifier("preproc_ifdef")),
+        Ok(Token::LParen),
+        Ok(Token::Identifier("declaration")),
+        Ok(Token::Identifier("declarator")),
+        Ok(Token::Colon),
+        Ok(Token::LParen),
+        Ok(Token::Identifier("init_declarator")),
+        Ok(Token::RParen),
+        Ok(Token::RParen),
+        Ok(Token::Capture("decl")),
+        Ok(Token::RParen),
+        Ok(Token::RParen),
+        Ok(Token::Warn),
+        Ok(Token::Capture("decl")),
+        Ok(Token::String("Global variable detected".to_string())),
+        Ok(Token::Semicolon),
+    ],
+)]
+fn test_token(#[case] source: &str, #[case] expected: Vec<Result<Token, TokenError>>) {
+    let actual = Token::lexer(source).collect::<Vec<_>>();
+    similar_asserts::assert_eq!(actual, expected);
+}
+
+#[rstest]
+#[case("ð´¿\u{0081}\x00\r$�dXåó©\u{0098}®\x08&")]
+fn test_string(#[case] source: &str) {
+    let tokens = StringToken::lexer(source)
+        .map(|result| {
+            assert!(matches!(result, Ok(_)));
+            result.unwrap()
+        })
+        .collect::<Vec<_>>();
+
+    similar_asserts::assert_eq!(
+        tokens,
+        source.chars().map(StringToken::Other).collect::<Vec<_>>()
+    );
+}
+
+#[allow(unused_imports)]
+mod proptests {
+    use super::*;
+    use proptest::{char, collection::vec, prop_assert, prop_assert_eq, proptest};
+
+    proptest! {
+        #[test]
+        fn doesnt_crash(source in ".*") {
+            run_lexer_impl(source.as_str(), false);
+        }
+
+        #[test]
+        fn string_escape(source in r#"(\\[bfnrt\\/"])*"#) {
+            let mut tokens = StringToken::lexer(source.as_str());
+            prop_assert!(tokens.all(|token| matches!(token, Ok(StringToken::Escape(_)))));
+        }
+
+        #[test]
+        fn string_unicode(codepoints in proptest::collection::vec(char::range('\u{1}', '\u{FFFF}'), 0..100)) {
+            let source = codepoints
+                .iter()
+                .map(|&c| format!("\\u{:04X?}", c as u32))
+                .collect::<String>();
+            let expected = codepoints
+                .iter()
+                .map(|&c| Ok(StringToken::Unicode(c)))
+                .collect::<Vec<_>>();
+            let tokens = StringToken::lexer(source.as_str()).collect::<Vec<_>>();
+            prop_assert_eq!(tokens, expected);
+        }
+
+        #[test]
+        fn string_other(source in r#"[^\\"]*"#) {
+            let mut tokens = StringToken::lexer(source.as_str());
+            prop_assert!(tokens.all(|token| matches!(token, Ok(StringToken::Other(_)))));
+        }
+    }
 }
